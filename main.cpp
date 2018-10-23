3a87a614426b12fb97e66c6b1032077f9a288c34
WhoDoIt <danil-21.2@yandex.ru>
2018-10-23 12:35:18 +0300

    Start of 2nd day

--- main.cpp
+++ main.cpp
@@ -3,20 +3,43 @@
 #include <iostream>
 
 #define PI 3.1415926
+#define T 4.3333f
+
 
 using namespace sf;
+
+
 
 int main()
 {
     RenderWindow window;
     window.create(VideoMode(1024, 768), "IT\'s works");
-    window.setSize(1024.f, 768.f);
+    window.setSize(Vector2u(1024.f, 768.f));
     
-    RectangleShape shape;
-    shape.setSize(Vector2f(75.f, 50.f));
-    shape.setOrigin(Vector2f(37.5f, 25.f));
-    shape.setPosition(Vector2f(50, 50));
-    shape.setFillColor(Color::Green);
+    RectangleShape player1;
+    player1.setSize(Vector2f(75.f, 50.f));
+    player1.setOrigin(Vector2f(37.5f, 25.f));
+    player1.setPosition(Vector2f(50, 50));
+    player1.setFillColor(Color::Green);
+
+    RectangleShape player2;
+    player2.setSize(Vector2f(75.f, 50.f));
+    player2.setOrigin(Vector2f(37.5f, 25.f));
+    player2.setPosition(Vector2f(50, 50));
+    player2.setFillColor(Color::Red);
+
+    CircleShape ball(15.f);
+    ball.setOrigin(Vector2f(25.f, 25.f));
+    ball.setPosition(Vector2f(512.f, 384.f));
+
+    sf::ConvexShape polygon;
+polygon.setPointCount(3);
+polygon.setPoint(0, sf::Vector2f(0, 0));
+polygon.setPoint(1, sf::Vector2f(0, 10));
+polygon.setPoint(2, sf::Vector2f(25, 5));
+polygon.setOutlineColor(sf::Color::Red);
+polygon.setOutlineThickness(5);
+polygon.setPosition(10, 20);
 
     while (window.isOpen())
     {
@@ -27,16 +50,27 @@ int main()
                 window.close();
         }
 
-        shape.rotate( 0.05 * (Keyboard::isKeyPressed(Keyboard::D) - Keyboard::isKeyPressed(Keyboard::A)) );
+        
+        player1.rotate( 0.05 * T * (Keyboard::isKeyPressed(Keyboard::D) - Keyboard::isKeyPressed(Keyboard::A)) );
+        
+        Vector2f behind (cos(player1.getRotation()*PI/180.f) * 0.05f,  sin(player1.getRotation()*PI/180.f) * 0.05f);
+        
+        player1.move(T * behind * (float)(Keyboard::isKeyPressed(Keyboard::S) - Keyboard::isKeyPressed(Keyboard::W)));
+		
 
-        Vector2f behind (cos(shape.getRotation()*PI/180.f) * 0.05f,  sin(shape.getRotation()*PI/180.f) *0.05f);
+		player2.rotate( 0.05 * T * (Keyboard::isKeyPressed(Keyboard::L) - Keyboard::isKeyPressed(Keyboard::J)) );
 
+        Vector2f behind2 (cos(player2.getRotation()*PI/180.f) * 0.05f,  sin(player2.getRotation()*PI/180.f) * 0.05f);
 
-        shape.move(behind * (float)(Keyboard::isKeyPressed(Keyboard::S) - Keyboard::isKeyPressed(Keyboard::W)));
+        player2.move(T * behind2 * (float)(Keyboard::isKeyPressed(Keyboard::K) - Keyboard::isKeyPressed(Keyboard::I)));
+		
 
         window.clear();
 
-        window.draw(shape);
+        window.draw(player1);
+        window.draw(player2);
+        window.draw(ball);
+        window.draw(polygon);
 
         window.display();
     }
--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
-g++ -c main.cpp -I~/Загрузки/SFML-2.5.1/include
-g++ main.o -o main -L~/Загрузки/SFML-2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system
+g++ -c *.cpp -I~/Загрузки/SFML-2.5.1/include
+g++ *.o -o main -L~/Загрузки/SFML-2.5.1/lib -lsfml-graphics -lsfml-window -lsfml-system
